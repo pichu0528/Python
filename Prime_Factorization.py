6ee560c02c8dd8d78141b0feb381192f1262da53
Pin Chu <pichu0528@gmail.com>
2018-12-10 13:50:35 -0800

    Create Prime_Factorization.py

---
+++ Prime_Factorization.py
@@ -0,0 +1,26 @@
+'''
+Find all Prime Facotrs(if there are any) and display them.
+'''
+
+def isPrime(i):
+    for j in range(2,i-1):
+        if i % j == 0:
+            return False
+    return True
+
+def prime_factorization(n):
+    if n < 2:
+        return '{} is not a prime number'.format(n)
+    
+    factor = []
+    prime_factor = []
+    for i in range(2,n+1):
+        if n % i == 0:
+            factor.append(i)
+
+    for n in factor:
+        if isPrime(n):
+            prime_factor.append(n)
+            continue
+            
+    return prime_factor
