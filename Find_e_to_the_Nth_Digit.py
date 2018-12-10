544d21bc08ccab14b87f97d694900eb293bed2b0
Pin Chu <pichu0528@gmail.com>
2018-12-09 17:50:02 -0800

    Update Find_e_to_the_Nth_Digit.py

--- Find_e_to_the_Nth_Digit.py
+++ Find_e_to_the_Nth_Digit.py
@@ -4,4 +4,20 @@ Enter a number and have the program generate e up to that many decimal places.
 Keep a limit to how far the program will go.
 '''
 
-# Method 1: 
+# Method 1: Using a list
+# Doesn't round up the result
+from math import e
+e_str = str(e)
+e_list = ['2','.']
+
+num_decimals = int(input('How many decimals: '))
+
+for x in range(2,num_decimals+2):
+    e_list.append(e_str[x])
+
+print(''.join(e_list))
+
+# Method 2: format string
+# Does round up the result
+num_decimals = int(input('How many decimals: '))
+print('{:.{}f}'.format(e,num_decimals))
