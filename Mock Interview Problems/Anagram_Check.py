51ec7963302ddd4bc2c7308400a8bc6bbddc664e
Pin Chu <pichu0528@gmail.com>
2018-12-30 14:30:30 -0800

    Update Anagram_Check.py

--- Mock Interview Problems/Anagram_Check.py
+++ Mock Interview Problems/Anagram_Check.py
@@ -13,7 +13,7 @@ For example:
 
 Note: Ignore spaces and capitalization. So "d go" is an anagram of "God" and "dog" and "o d g".
 '''
-# My solution
+# My solution 1 - compare the result for each string if they have the same characters in sorted order
 def anagram(s1,s2):
     s1_list = list(''.join(s1.lower().split()))
     s2_list = list(''.join(s2.lower().split()))
@@ -22,6 +22,37 @@ def anagram(s1,s2):
         return True
     else:
         return False
+
+# My solution 2 - using dictionary to keep the count of each character, and compare the counts of each character
+def anagram2(s1,s2):
+    s1 = s1.replace(' ','').lower()
+    s2 = s2.replace(' ','').lower()
+    
+    s1_dict = dict()
+    s2_dict = dict()
+    
+    for a in s1:
+        if not a in s1_dict:
+            s1_dict[a] = 1
+        else:
+            s1_dict[a] += 1
+    
+    for b in s2:
+        if not b in s2_dict:
+            s2_dict[b] = 1
+        else:
+            s2_dict[b] += 1
+            
+    for c in s1_dict:
+        try:
+            if s1_dict[c] == s2_dict[c]:
+                continue
+            else:
+                return False
+        
+        except Exception:
+            return False
+    return True
         
 '''
 RUN THIS CELL TO TEST YOUR SOLUTION
