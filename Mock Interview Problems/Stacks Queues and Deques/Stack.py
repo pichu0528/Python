857f5ab239f6b6f0ae2c42507296a5b2f115f3f9
Pin Chu <pichu0528@gmail.com>
2019-01-08 11:14:16 -0800

    Create Stack.py

---
+++ Mock Interview Problems/Stacks Queues and Deques/Stack.py
@@ -0,0 +1,26 @@
+'''
+- Check if its empty
+- Push a new item
+- Pop an item
+- Peek at the top item
+- Return the size
+'''
+class Stack(object):
+    
+    def __init__(self):
+        self.items = []
+        
+    def isEmpty(self):
+        return self.items == []
+    
+    def push(self,item):
+        self.items.append(item)
+        
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[len(self.items) - 1]
+    
+    def size(self):
+        return len(self.items)
