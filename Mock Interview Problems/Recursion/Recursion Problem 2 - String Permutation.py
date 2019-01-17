4263820b1e07cfd2eecb10c9125f9e6f1228e986
Pin Chu <pichu0528@gmail.com>
2019-01-17 15:53:33 -0800

    Update Recursion Problem 2 - String Permutation.py

--- Mock Interview Problems/Recursion/Recursion Problem 2 - String Permutation.py
+++ Mock Interview Problems/Recursion/Recursion Problem 2 - String Permutation.py
@@ -6,22 +6,43 @@ For example, given s='abc' the function should return ['abc', 'acb', 'bac', 'bca
 
 Note: If a character is repeated, treat each occurence as distinct, for example an input of 'xxx' 
 would return a list with 6 "versions" of 'xxx'
-
-1. Iterate through the initial string – e.g., ‘abc’.
-
-2. For each character in the initial string, set aside that character and get a list of all 
-permutations of the string that’s left. So, for example, if the current iteration is on 'b', 
-we’d want to find all the permutations of the string 'ac'.
-
-3. Once you have the list from step 2, add each element from that list to the character from 
-the initial string, and append the result to our list of final results. So if we’re on 'b' 
-and we’ve gotten the list ['ac', 'ca'], we’d add 'b' to those, resulting in 'bac' and 'bca', 
-each of which we’d add to our final results.
-
-4. Return the list of final results.
 '''
+# Solution 1 - with counter
+# Bascially, each permutation is a pair of the string s and the reverse of the string.
+# example: abc -> cba, 
+#          bca -> acb, 
+#          cab -> bac
+# So, the goal here is to find all the possible pairs. 
+# We will keep a counter to keep track of how many pairs we have so far. 
+# Total pair should be the factorial of len(n) divided by 2
+# Will need to make sure of the math library to do the factorial count.
+import math
+def permute1(s, count=0):
+    l = []
+    
+    #base case
+    if count == math.factorial(len(s))/2:
+        return []
+    
+    l.append(s)
+    l.append(s[::-1])
+    return sorted(l + permute1(s[1:] + s[0], count+1))
 
-def permute(s):
+
+# Solution 2 - without counter
+# 1. Iterate through the initial string – e.g., ‘abc’.
+#
+# 2. For each character in the initial string, set aside that character and get a list of all 
+# permutations of the string that’s left. So, for example, if the current iteration is on 'b', 
+# we’d want to find all the permutations of the string 'ac'.
+#
+# 3. Once you have the list from step 2, add each element from that list to the character from 
+# the initial string, and append the result to our list of final results. So if we’re on 'b' 
+# and we’ve gotten the list ['ac', 'ca'], we’d add 'b' to those, resulting in 'bac' and 'bca', 
+# each of which we’d add to our final results.
+#
+# 4. Return the list of final results.
+def permute2(s):
     out = []
     
     # Base Case
@@ -33,7 +54,7 @@ def permute(s):
         for i, let in enumerate(s):
             
             # For every permutation resulting from Step 2 and 3 described above
-            for perm in permute(s[:i] + s[i+1:]):
+            for perm in permute2(s[:i] + s[i+1:]):
                 
                 # Add it to output
                 out += [let + perm]
@@ -58,4 +79,5 @@ class TestPerm(object):
         
 # Run Tests
 t = TestPerm()
-t.test(permute)
+t.test(permute1)
+t.test(permute2)
