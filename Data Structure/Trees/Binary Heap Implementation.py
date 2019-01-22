3b30ae5440b794864a597dcb9576d9db76888348
Pin Chu <pichu0528@gmail.com>
2019-01-22 09:30:41 -0800

    Update Binary Heap Implementation.py

--- Data Structure/Trees/Binary Heap Implementation.py
+++ Data Structure/Trees/Binary Heap Implementation.py
@@ -3,12 +3,15 @@ Binary Heap Operations
 The basic operations we will implement for our binary heap are as follows:
 
 BinaryHeap() creates a new, empty, binary heap.
-insert(k) adds a new item to the heap.
 findMin() returns the item with the minimum key value, leaving item in the heap.
-delMin() returns the item with the minimum key value, removing the item from the heap.
 isEmpty() returns true if the heap is empty, false otherwise.
 size() returns the number of items in the heap.
-buildHeap(list) builds a new heap from a list of keys.
+
+- important methods
+    insert(k) adds a new item to the heap.
+    delMin() returns the item with the minimum key value, removing the item from the heap.
+    buildHeap(list) builds a new heap from a list of keys.
+
 '''
 class BinHeap(object):
     
