290bf18ab9be8abc64be3cf378659c4960fd9eb5
Pin Chu <pichu0528@gmail.com>
2019-01-22 09:25:58 -0800

    Create Binary Heap Implementation.py

---
+++ Data Structure/Trees/Binary Heap Implementation.py
@@ -0,0 +1,61 @@
+class BinHeap(object):
+    
+    def __init__(self):
+        self.heapList = [0] # 0 as a place holder because we want to start our index at 1 for arithmetic reason
+        self.currentSize = 0
+        
+    def percUp(self,i):
+        while i//2 > 0:
+            if self.heapList[i] < self.heapList[i//2]:
+                temp = self.heapList[i//2]
+                self.heapList[i//2] = self.heapList[i]
+                self.heapList[i] = temp
+            i = i//2
+    
+    def insert(self,k):
+        self.heapList.append(k)
+        self.currentSize += 1
+        self.percUp(self.currentSize)
+    
+    def percDown(self,i):
+        while (i * 2) <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]:
+                temp = self.heapList[i]
+                self.heapList[i] = self.heapList[mc]
+                self.heapList[mc] = temp
+            i = mc
+            
+    def minChild(self,i):
+        if (i*2) + 1 > self.currentSize:
+            return i*2
+        else:
+            if self.heapList[i*2] < self.heapList[i*2 + 1]:
+                return i*2
+            else:
+                return i*2 + 1
+    
+    def delMin(self):
+        removeNode = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize -= 1
+        self.heapList.pop()
+        self.percDown(1)        
+        return removeNode
+    
+    def findMin(self):
+        return self.heapList[1]
+    
+    def isEmpty(self):
+        return self.currentSize == 0
+    
+    def size(self):
+        return self.currentSize
+    
+    def buildHeap(self,alist):
+        i = len(alist)//2
+        self.currentSize = len(alist)
+        self.heapList = [0] + alist
+        while i > 0:
+            self.percDown(i)
+            i -= 1
