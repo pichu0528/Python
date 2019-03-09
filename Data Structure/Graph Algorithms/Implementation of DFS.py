9e7f1e6670835c164f9c66957e24fc75ccc8abb5
Pin Chu <pichu0528@gmail.com>
2019-03-09 15:49:25 -0800

    Update Implementation of DFS.py

--- Data Structure/Graph Algorithms/Implementation of DFS.py
+++ Data Structure/Graph Algorithms/Implementation of DFS.py
@@ -6,6 +6,7 @@ graph = {'A': set(['B', 'C']),
          'D': set(['B']),
          'E': set(['B', 'F']),
          'F': set(['C', 'E'])}
+Note: the vertices is in a set
 '''
 
 def dfs(graph,start):
@@ -21,5 +22,31 @@ def dfs(graph,start):
             
             # the subtract is only available for sets
             stack.extend(graph[vertex] - visited)
+            
+    return visited
+
+'''
+Given
+graph = {'A': ['B', 'C'],
+         'B': ['A', 'D', 'E'],
+         'C': ['A', 'F'],
+         'D': ['B'],
+         'E': ['B', 'F'],
+         'F': ['C', 'E']}
+         
+Note: the vertices are in a list
+'''
+def dfs(graph,start):
+    # all visited vertices are put in the list
+    visited = []
+    stack = [start]
+    
+    while stack:
+        vertex = stack.pop()
+        
+        if vertex not in visited:
+            visited.append(vertex)
+            
+            stack.extend([v for v in graph[vertex] if v not in visited])
             
     return visited
